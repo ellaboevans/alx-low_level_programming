df3ef1783b5d2fcaf146e97a71a1d54d148faa48
Evans Elabo <ellaboevans@gmail.com>
2023-06-15 03:45:55 -0700

    Fix bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+* main - Main Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	/* This function prints a texts */
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
