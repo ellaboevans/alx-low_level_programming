6123f0fa5cdb328acbc3e9793ef48f8c1da97bb7
Evans Elabo <ellaboevans@gmail.com>
2023-06-20 16:19:07 -0700

    fix code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if ( k < 0 && j != 0)
+			if (k < 0 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -32,5 +32,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
