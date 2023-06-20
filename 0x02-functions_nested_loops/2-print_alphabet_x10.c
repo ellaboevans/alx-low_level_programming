75039f9a5422820f478ba11e92ed8527a92f2ba6
Evans Elabo <ellaboevans@gmail.com>
2023-06-20 09:28:49 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Entry point
+  * Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	_putchar('\n');
+	}
