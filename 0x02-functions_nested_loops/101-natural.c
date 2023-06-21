0633cbf75f911e23a43a4c8947997734be4e8f5f
Evans Elabo <ellaboevans@gmail.com>
2023-06-20 17:34:19 -0700

    omputes and prints the sum of all the multiples of anum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+  * main - Entry point
+  * Description: computes and prints the sum
+  * of all the multiples of a number
+  * Return: Always 0
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum = sum + num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
