86ff57e0d7801f3af82fceb722dde61a2bfcddd1
Evans Elabo <ellaboevans@gmail.com>
2023-06-22 17:51:37 -0700

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints the numbers
+  * Return: 0
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num < '10'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+
+	return (0);
+}
