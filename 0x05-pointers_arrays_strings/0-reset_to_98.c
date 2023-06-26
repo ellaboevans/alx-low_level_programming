2d8463eb2fb89dd76b97bba233725a405f650405
Evans Elabo <ellaboevans@gmail.com>
2023-06-26 09:14:02 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#incldue "main.h"
+
+/**
+  * reset_to_98 - a function that takes pointer
+  * to an int as a parameter
+  * @n: input
+  * Return: n
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
